17f530a3593cc0937054f7277e2216f2a3ee2f12
Johan Goossens <johan@cox.net>
2020-08-02 21:15:11 -0400

    Improved Path class methods

--- modules/system/OtPath.h
+++ modules/system/OtPath.h
@@ -47,20 +47,41 @@ public:
 		return getSharedPtr();
 	}
 
+	// modifiers
+	void clear() { path.clear(); }
+	void removeFilename() { path.remove_filename(); }
+	void replaceFilename(OtObject name) { path.replace_filename(name->operator std::wstring()); }
+	void replaceExtension(OtObject name) { path.replace_extension(name->operator std::wstring()); }
+
 	// operators
 	OtObject add(OtObject operand) { return OtPathClass::create(path / operand->operator std::wstring()); }
 	bool equal(OtObject operand) { return path == operand->operator std::wstring(); }
-	bool notEqual(OtObject operand) { return path != operand->operator std::wstring(); }
-	bool greaterThan(OtObject operand) { return path > operand->operator std::wstring(); }
-	bool lessThan(OtObject operand) { return path < operand->operator std::wstring(); }
-	bool greaterEqual(OtObject operand) { return path >= operand->operator std::wstring(); }
-	bool lessEqual(OtObject operand) { return path <= operand->operator std::wstring(); }
+	bool notEqual(OtObject operand) { return !equal(operand); }
+
+	// check path status
+	bool isEmpty() { return path.empty(); }
+	bool isAbsolute() { return path.is_absolute(); }
+	bool isRelative() { return path.is_relative(); }
+
+	bool hasRootPath() { return path.has_root_path(); }
+	bool hasRootName() { return path.has_root_name(); }
+	bool hasRootDirectory() { return path.has_root_directory(); }
+	bool hasRelativePath() { return path.has_relative_path(); }
+	bool hasParentPath() { return path.has_parent_path(); }
+	bool hasFilename() { return path.has_filename(); }
+	bool hasStem() { return path.has_stem(); }
+	bool hasExtension() { return path.has_extension(); }
 
 	// get path elements
+	OtObject rootName() { return create(path.root_name()); }
+	OtObject rootDirectory() { return create(path.root_directory()); }
+	OtObject rootPath() { return create(path.root_path()); }
 	OtObject directory() { return create(path.parent_path()); }
 	OtObject filename() { return create(path.filename()); }
 	OtObject stem() { return create(path.stem()); }
 	OtObject extension() { return create(path.extension()); }
+
+	OtObject relative(OtObject name) { return create(path.lexically_relative(name->operator std::wstring())); }
 
 	// support iterator
 	class OtPathIteratorClass : public OtInternalClass {
@@ -108,15 +129,34 @@ public:
 
 			type->set(L"__eq__", OtFunctionCreate(&OtPathClass::equal));
 			type->set(L"__ne__", OtFunctionCreate(&OtPathClass::notEqual));
-			type->set(L"__gt__", OtFunctionCreate(&OtPathClass::greaterThan));
-			type->set(L"__lt__", OtFunctionCreate(&OtPathClass::lessThan));
-			type->set(L"__ge__", OtFunctionCreate(&OtPathClass::greaterEqual));
-			type->set(L"__le__", OtFunctionCreate(&OtPathClass::lessEqual));
 
+			type->set(L"clear", OtFunctionCreate(&OtPathClass::clear));
+			type->set(L"removeFilename", OtFunctionCreate(&OtPathClass::removeFilename));
+			type->set(L"replaceFilename", OtFunctionCreate(&OtPathClass::replaceFilename));
+			type->set(L"replaceExtension", OtFunctionCreate(&OtPathClass::replaceExtension));
+
+			type->set(L"isEmpty", OtFunctionCreate(&OtPathClass::isEmpty));
+			type->set(L"isAbsolute", OtFunctionCreate(&OtPathClass::isAbsolute));
+			type->set(L"isRelative", OtFunctionCreate(&OtPathClass::isRelative));
+
+			type->set(L"hasRootPath", OtFunctionCreate(&OtPathClass::hasRootPath));
+			type->set(L"hasRootName", OtFunctionCreate(&OtPathClass::hasRootName));
+			type->set(L"hasRootDirectory", OtFunctionCreate(&OtPathClass::hasRootDirectory));
+			type->set(L"hasRelativePath", OtFunctionCreate(&OtPathClass::hasRelativePath));
+			type->set(L"hasParentPath", OtFunctionCreate(&OtPathClass::hasParentPath));
+			type->set(L"hasFilename", OtFunctionCreate(&OtPathClass::hasFilename));
+			type->set(L"hasStem", OtFunctionCreate(&OtPathClass::hasStem));
+			type->set(L"hasExtension", OtFunctionCreate(&OtPathClass::hasExtension));
+
+			type->set(L"rootName", OtFunctionCreate(&OtPathClass::rootName));
+			type->set(L"rootDirectory", OtFunctionCreate(&OtPathClass::rootDirectory));
+			type->set(L"rootPath", OtFunctionCreate(&OtPathClass::rootPath));
 			type->set(L"directory", OtFunctionCreate(&OtPathClass::directory));
 			type->set(L"filename", OtFunctionCreate(&OtPathClass::filename));
 			type->set(L"stem", OtFunctionCreate(&OtPathClass::stem));
 			type->set(L"extension", OtFunctionCreate(&OtPathClass::extension));
+
+			type->set(L"", OtFunctionCreate(&OtPathClass::relative));
 		}
 
 		return type;

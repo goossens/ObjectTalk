c60e887ec0ec23382f69988d6614e23be7150fa8
Johan Goossens <johan@cox.net>
2022-02-08 16:12:08 -0500

    Add debug function to measure execution time

---
+++ ot/utility/OtMeasure.h
@@ -0,0 +1,31 @@
+//	ObjectTalk Scripting Language
+//	Copyright (c) 1993-2022 Johan A. Goossens. All rights reserved.
+//
+//	This work is licensed under the terms of the MIT license.
+//	For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+#pragma once
+
+
+//
+//	Include files
+//
+
+#include <chrono>
+#include <utility>
+
+
+//
+//	Measure time to execute function
+//
+
+// auto OtMeasureFunction = [](auto&& func, auto&&... params) {
+
+template<typename F, typename... Args>
+double OtMeasureFunction(F func, Args&&... args){
+	const auto& start = std::chrono::high_resolution_clock::now();
+	std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...);
+	const auto& stop = std::chrono::high_resolution_clock::now();
+	return std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+}

43c65d6202be7a6be5da0ef034b4f77855ffac20
Johan Goossens <johan@cox.net>
2022-11-26 08:55:38 -0500

    Better handle textures with transparency

--- gui/shaders/textured/shader.fs
+++ gui/shaders/textured/shader.fs
@@ -16,6 +16,11 @@ void main() {
 	// get texture color
 	vec4 color = texture2D(s_texture, v_texcoord0);
 
+	// discard pixel if too transparent
+	if (color.w < 0.05) {
+		discard;
+	}
+
 	// return fragment color
 	gl_FragColor = applyLight(color, v_position, v_normal, v_shadow);
 }

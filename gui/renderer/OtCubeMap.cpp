a5afa3f3fd0291aa69b8d0f289705c513800e36b
Johan Goossens <johan@cox.net>
2023-02-17 12:44:03 -0500

    Fixed cubemap loading error

--- gui/renderer/OtCubeMap.cpp
+++ gui/renderer/OtCubeMap.cpp
@@ -38,60 +38,67 @@ OtCubeMap::~OtCubeMap() {
 
 void OtCubeMap::load(const std::string &posx, const std::string &negx, const std::string &posy, const std::string &negy, const std::string &posz, const std::string &negz) {
 	// load first side
-	bimg::ImageContainer* image = OtImage(posx, true, true).getContainer();
-	uint16_t imageSize = image->m_width;
-	bimg::TextureFormat::Enum imageFormat = image->m_format;
+	OtImage image;
+	image.load(posx, true, true);
+	bimg::ImageContainer* container = image.getContainer();
+	uint16_t imageSize = container->m_width;
+	bimg::TextureFormat::Enum imageFormat = container->m_format;
 
 	// create a new cubemap
 	cubemap = bgfx::createTextureCube(imageSize, false, 1, bgfx::TextureFormat::Enum(imageFormat), BGFX_TEXTURE_NONE | BGFX_SAMPLER_NONE);
 
 	// store first side
-	const bgfx::Memory* mem = bgfx::copy(image->m_data, image->m_size);
+	const bgfx::Memory* mem = bgfx::copy(container->m_data, container->m_size);
 	bgfx::updateTextureCube(cubemap.getHandle(), 0, 0, 0, 0, 0, imageSize, imageSize, mem);
 
 	// load and store other sides
-	image = OtImage(negx, true, true).getContainer();
+	image.load(negx, true, true);
+	container = image.getContainer();
 
-	if (image->m_width != imageSize || image->m_format != imageFormat) {
+	if (container->m_width != imageSize || container->m_format != imageFormat) {
 		OtExcept("Cubemap image (negx] does not have same size or format as others");
 	}
 
-	mem = bgfx::copy(image->m_data, image->m_size);
+	mem = bgfx::copy(container->m_data, container->m_size);
 	bgfx::updateTextureCube(cubemap.getHandle(), 0, 1, 0, 0, 0, imageSize, imageSize, mem);
 
-	image = OtImage(posy, true, true).getContainer();
+	image.load(posy, true, true);
+	container = image.getContainer();
 
-	if (image->m_width != imageSize || image->m_format != imageFormat) {
+	if (container->m_width != imageSize || container->m_format != imageFormat) {
 		OtExcept("Cubemap image (posy] does not have same size or format as others");
 	}
 
-	mem = bgfx::copy(image->m_data, image->m_size);
+	mem = bgfx::copy(container->m_data, container->m_size);
 	bgfx::updateTextureCube(cubemap.getHandle(), 0, 2, 0, 0, 0, imageSize, imageSize, mem);
 
-	image = OtImage(negy, true, true).getContainer();
+	image.load(negy, true, true);
+	container = image.getContainer();
 
-	if (image->m_width != imageSize || image->m_format != imageFormat) {
+	if (container->m_width != imageSize || container->m_format != imageFormat) {
 		OtExcept("Cubemap image (negy] does not have same size or format as others");
 	}
 
-	mem = bgfx::copy(image->m_data, image->m_size);
+	mem = bgfx::copy(container->m_data, container->m_size);
 	bgfx::updateTextureCube(cubemap.getHandle(), 0, 3, 0, 0, 0, imageSize, imageSize, mem);
 
-	image = OtImage(posz, true, true).getContainer();
+	image.load(posz, true, true);
+	container = image.getContainer();
 
-	if (image->m_width != imageSize || image->m_format != imageFormat) {
+	if (container->m_width != imageSize || container->m_format != imageFormat) {
 		OtExcept("Cubemap image (posz] does not have same size or format as others");
 	}
 
-	mem = bgfx::copy(image->m_data, image->m_size);
+	mem = bgfx::copy(container->m_data, container->m_size);
 	bgfx::updateTextureCube(cubemap.getHandle(), 0, 4, 0, 0, 0, imageSize, imageSize, mem);
 
-	image = OtImage(negz, true, true).getContainer();
+	image.load(negz, true, true);
+	container = image.getContainer();
 
-	if (image->m_width != imageSize || image->m_format != imageFormat) {
+	if (container->m_width != imageSize || container->m_format != imageFormat) {
 		OtExcept("Cubemap image (negz] does not have same size or format as others");
 	}
 
-	mem = bgfx::copy(image->m_data, image->m_size);
+	mem = bgfx::copy(container->m_data, container->m_size);
 	bgfx::updateTextureCube(cubemap.getHandle(), 0, 5, 0, 0, 0, imageSize, imageSize, mem);
 }

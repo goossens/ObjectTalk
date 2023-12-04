ddc5bfda410bcd07ee218924db4c982484bf4db4
Johan Goossens <johan@cox.net>
2023-12-04 18:23:41 -0500

    Fixed terrain horizontal scaling

--- gfx/framework/OtBGFX.cpp
+++ gfx/framework/OtBGFX.cpp
@@ -75,7 +75,8 @@ void OtFramework::initBGFX() {
 	init.type = bgfx::RendererType::Direct3D12;
 
 #else
-	init.type = bgfx::RendererType::Vulkan;
+	init.type = bgfx::RendererType::OpenGL;
+//	init.type = bgfx::RendererType::Vulkan;
 #endif
 
 	init.platformData.nwh = nativeDisplayHandle;

2fe8e9122b4136f21948338ca09f72c7feabda8a
Johan Goossens <johan@cox.net>
2024-08-30 08:57:05 -0400

    Improved debugger command line (history, auto-complete and help)

--- ot/language/OtDebugger.cpp
+++ ot/language/OtDebugger.cpp
@@ -69,6 +69,45 @@ std::string OtDebuggerClass::disassemble() {
 
 
 //
+//	Completer support functions
+//
+
+static void wordCompleter(ic_completion_env_t* cenv, const char* word) {
+	static const char* completions[] = {
+		"help",
+		"where",
+		"disassemble",
+		"continue",
+		"step",
+		"in",
+		"out",
+		"quit",
+		nullptr};
+
+	ic_add_completions(cenv, word, completions);
+}
+
+static void completer(ic_completion_env_t* cenv, const char* input) {
+	ic_complete_word(cenv, input, &wordCompleter, nullptr);
+}
+
+
+//
+//	Help text
+//
+
+static const char* help =
+	"[ansi-blue]help[/]        show this help\n"
+	"[ansi-blue]where[/]       show the current source line\n"
+	"[ansi-blue]disassemble[/] disassemble the current function\n"
+	"[ansi-blue]continue[/]    continue the program\n"
+	"[ansi-blue]step[/]        step over the current instruction\n"
+	"[ansi-blue]in[/]          step into the next function\n"
+	"[ansi-blue]out[/]         step out of the current function\n"
+	"[ansi-blue]quit[/]        terminate the debugger\n";
+
+
+//
 //	OtDebuggerClass::processCommand
 //
 
@@ -77,6 +116,15 @@ void OtDebuggerClass::processCommand() {
 	bool debugging = true;
 
 	while (debugging) {
+		// initialize command line library (if required)
+		static bool initialized = false;
+
+		if (!initialized) {
+			ic_set_default_completer(&completer, nullptr);
+			ic_set_history(nullptr, -1);
+			initialized = true;
+		}
+
 		// get command line input
 		char* input = ic_readline("ot");
 
@@ -91,7 +139,10 @@ void OtDebuggerClass::processCommand() {
 
 		if (command.size()) {
 			// process each command
-			if (command[0] == "where" || command[0] == "w") {
+			if (command[0] == "help" || command[0] == "h") {
+				ic_print(help);
+
+			} else if (command[0] == "where" || command[0] == "w") {
 				ic_print(where().c_str());
 
 			} else if (command[0] == "disassemble" || command[0] == "d") {

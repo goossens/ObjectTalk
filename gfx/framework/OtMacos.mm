83238a9176e76f196474f183ca2c50f5ad168585
Johan Goossens <johan@cox.net>
2025-08-15 07:25:37 -0400

    Cleaned up MacOS unique framework parts

--- gfx/framework/OtMacos.mm
+++ gfx/framework/OtMacos.mm
@@ -9,14 +9,7 @@
 //	Include files
 //
 
-#import <objc/runtime.h>
-
 #import <AppKit/AppKit.h>
-#import <Cocoa/Cocoa.h>
-#import <Foundation/Foundation.h>
-#import <Metal/Metal.h>
-#import <MetalKit/MetalKit.h>
-#import <QuartzCore/CAMetalLayer.h>
 
 #include "OtFramework.h"
 
@@ -28,13 +21,9 @@
 void OtFramework::fixMenus() {
 	// fix menu labels
 	NSString* appName = [NSString stringWithCString:"ObjectTalk" encoding:[NSString defaultCStringEncoding]];
-
 	NSMenu* appMenu = [[[NSApp mainMenu] itemAtIndex: 0] submenu];
 	[appMenu setTitle: appName];
 	[[appMenu itemAtIndex: 0] setTitle: [@"About " stringByAppendingString: appName]];
 	[[appMenu itemAtIndex: 6] setTitle: [@"Hide " stringByAppendingString: appName]];
 	[[appMenu itemAtIndex: 10] setTitle: [@"Quit " stringByAppendingString: appName]];
-
-	NSMenu* windowMenu = [[[NSApp mainMenu] itemAtIndex: 1] submenu];
-	[windowMenu removeItemAtIndex: 0];
 }

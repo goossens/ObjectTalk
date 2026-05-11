4384b227c308fb9979c7f78e92b43877e6922ab0
Johan Goossens <johan@cox.net>
2026-05-11 15:16:16 -0400

    Added ligtiff to load and save height maps

--- gfx/geo/OtHeightMap.cpp
+++ gfx/geo/OtHeightMap.cpp
@@ -11,14 +11,17 @@
 
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
 #include <cstring>
 #include <limits>
 #include <numbers>
 
+#include "tiffio.h"
+
 #include "OtAssert.h"
+#include "OtLog.h"
 
 #include "OtHeightMap.h"
-#include "OtImage.h"
 
 
 //
@@ -45,24 +48,120 @@ void OtHeightMap::update(int w, int h, bool clear) {
 
 
 //
+//	errorHandler
+//
+
+static int errorHandler(TIFF*, void* data, const char*, const char* format, va_list args) {
+	auto heightmap = static_cast<OtHeightMap*>(data);
+	char buffer[512];
+	vsnprintf(buffer, sizeof(buffer), format, args);
+	heightmap->setErrorMessage(buffer);
+	return 1;
+}
+
+
+//
 //	OtHeightMap::load
 //
 
-void OtHeightMap::load(const std::string& path, float minValue, float maxValue) {
-	// load height map from image
-	OtImage image;
-	image.load(path);
+void OtHeightMap::load(const std::string& path) {
+	// open file for read
+	auto options = TIFFOpenOptionsAlloc();
+	TIFFOpenOptionsSetErrorHandlerExtR(options, errorHandler, this);
+	auto tiff = TIFFOpenExt(path.c_str(), "r", options);
+	TIFFOpenOptionsFree(options);
 
-	// update heightmap (if required)
-	update(image.getWidth(), image.getHeight());
+	if (!tiff) {
+		OtLogError("Can't load from heightmap file [{}]: {}", path, errorMessage);
+	}
 
-	// copy pixels
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			auto pixel = image.getPixelGray(x, y);
-			setHeight(x, y, std::lerp(minValue, maxValue, pixel));
+	// sanity checks
+	uint32_t rows;
+	uint32_t columns;
+	uint16_t sampleFormat;
+	uint16_t componentsPerPixel;
+
+	if (TIFFGetField(tiff, TIFFTAG_IMAGELENGTH, &rows) != 1) { OtLogError("[TIFFTAG_IMAGELENGTH] failed: {}", errorMessage); }
+	if (TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &columns) != 1) { OtLogError("[TIFFTAG_IMAGEWIDTH] failed: {}", errorMessage); }
+	if (TIFFGetField(tiff, TIFFTAG_SAMPLEFORMAT, &sampleFormat) != 1) { OtLogError("[TIFFTAG_SAMPLEFORMAT] failed: {}", errorMessage); }
+	if (TIFFGetField(tiff, TIFFTAG_SAMPLESPERPIXEL, &componentsPerPixel) != 1) { OtLogError("[TIFFTAG_SAMPLESPERPIXEL] failed: {}", errorMessage); }
+
+	if (rows < 1) {
+		OtLogError("Invalid TIFF file [{}]: height [{}] < 1", path, rows);
+	}
+
+	if (columns < 1) {
+		OtLogError("Invalid TIFF file [{}]: width [{}] < 1", path, rows);
+	}
+
+	if (componentsPerPixel != 1) {
+		OtLogError("Unsupported TIFF file [{}]: too many components per pixel [{}]", path, componentsPerPixel);
+	}
+
+	if (sampleFormat == SAMPLEFORMAT_IEEEFP) {
+		update(static_cast<int>(columns), static_cast<int>(rows));
+		auto p = heightmap.get();
+
+		for (uint32_t row = 0; row < rows; row++) {
+			if (TIFFReadScanline(tiff, p, row) != 1) {
+				OtLogError("Error while reading from TIFF file [{}]: {}", path, errorMessage);
+			}
+
+			p += columns;
+		}
+
+	} else {
+		OtLogError("Unsupported TIFF file [{}]: wrong format [{}]", path, sampleFormat);
+	}
+
+	TIFFClose(tiff);
+}
+
+
+//
+//	OtHeightMap::save
+//
+
+void OtHeightMap::save(const std::string& path) {
+	if (!heightmap) {
+		OtLogError("Can't save invalid heightmap");
+	}
+
+	// open file for write
+	auto options = TIFFOpenOptionsAlloc();
+	TIFFOpenOptionsSetErrorHandlerExtR(options, errorHandler, this);
+	auto tiff = TIFFOpenExt(path.c_str(), "w", options);
+	TIFFOpenOptionsFree(options);
+
+	if (!tiff) {
+		OtLogError("Can't save to heightmap file [{}]", path);
+	}
+
+	// basic image dimensions
+	TIFFSetField(tiff, TIFFTAG_IMAGEWIDTH, width);
+	TIFFSetField(tiff, TIFFTAG_IMAGELENGTH, height);
+
+	// Heightmap configuration: 1 channel, 32-bit
+	TIFFSetField(tiff, TIFFTAG_SAMPLESPERPIXEL, 1);
+	TIFFSetField(tiff, TIFFTAG_BITSPERSAMPLE, 32);
+	TIFFSetField(tiff, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
+	TIFFSetField(tiff, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
+	TIFFSetField(tiff, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
+
+	// explicitly tell libtiff we are using unsigned integers
+	TIFFSetField(tiff, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_IEEEFP);
+
+	// write data row by row (scanline)
+	auto p = heightmap.get();
+	auto rows = static_cast<uint32_t>(height);
+
+	for (uint32_t row = 0; row < rows; row++) {
+		if (TIFFWriteScanline(tiff, p, row) != 1) {
+			OtLogError("Error while writing to TIFF file [{}]: {}", path, errorMessage);
 		}
 	}
+
+	TIFFClose(tiff);
 }
 
 
@@ -70,7 +169,7 @@ void OtHeightMap::load(const std::string& path, float minValue, float maxValue) 
 //	OtHeightMap::setHeight
 //
 
-void OtHeightMap::setHeight(int x, int y, float value) {
+void OtHeightMap::setHeight(int x, int y, float value) const {
 	// sanity checks
 	OtAssert(heightmap != nullptr);
 	OtAssert(x >= 0 && x < width);
@@ -85,7 +184,7 @@ void OtHeightMap::setHeight(int x, int y, float value) {
 //	OtHeightMap::getHeight
 //
 
-float OtHeightMap::getHeight(int x, int y) {
+float OtHeightMap::getHeight(int x, int y) const {
 	// sanity check
 	OtAssert(heightmap != nullptr);
 
@@ -100,7 +199,7 @@ float OtHeightMap::getHeight(int x, int y) {
 //	OtHeightMap::getHeight
 //
 
-float OtHeightMap::getHeight(float x, float y) {
+float OtHeightMap::getHeight(float x, float y) const {
 	x *= width - 1;
 	y *= height - 1;
 
@@ -124,7 +223,7 @@ float OtHeightMap::getHeight(float x, float y) {
 //	OtHeightMap::getNormal
 //
 
-glm::vec3 OtHeightMap::getNormal(float x, float y) {
+glm::vec3 OtHeightMap::getNormal(float x, float y) const {
 	int ix = int(x * width);
 	int iy = int(y * height);
 
@@ -139,7 +238,7 @@ glm::vec3 OtHeightMap::getNormal(float x, float y) {
 //	OtHeightMap::getMinMaxHeights
 //
 
-void OtHeightMap::getMinMaxHeights(float& minHeight, float& maxHeight) {
+void OtHeightMap::getMinMaxHeights(float& minHeight, float& maxHeight) const {
 	minHeight = std::numeric_limits<float>::max();
 	maxHeight = std::numeric_limits<float>::lowest();
 
--- gfx/geo/OtHeightMap.h
+++ gfx/geo/OtHeightMap.h
@@ -28,7 +28,12 @@ public:
 	void update(int w, int h, bool clear=false);
 
 	// load heightmap from file
-	void load(const std::string& path, float minValue, float maxValue);
+	void load(const std::string& path);
+
+	// save heightmap to file
+	void save(const std::string& path);
+
+	// void load(const std::string& path, float minValue, float maxValue);
 
 	// clear heightmap
 	inline void clear() {
@@ -38,26 +43,31 @@ public:
 	}
 
 	// see if heightmap is valid
-	inline bool isValid() { return heightmap != nullptr; }
+	inline bool isValid() const { return heightmap != nullptr; }
 
 	// set height at specified location
-	void setHeight(int x, int y, float value);
+	void setHeight(int x, int y, float value) const;
 
 	// get height at absolute location
-	float getHeight(int x, int y);
+	float getHeight(int x, int y) const;
 
 	// get height at specified location (in relative coordinates, 0.0 to 1.0)
-	float getHeight(float x, float y);
+	float getHeight(float x, float y) const;
 
 	// get normal (as normalized vector) at specified location (in relative coordinates, 0.0 to 1.0)
-	glm::vec3 getNormal(float x, float y);
+	glm::vec3 getNormal(float x, float y) const;
 
 	// get minimum and maximum heights
-	void getMinMaxHeights(float& minHeight, float& maxHeight);
+	void getMinMaxHeights(float& minHeight, float& maxHeight) const;
 
-	private:
+	// access the error message
+	inline void setErrorMessage(const std::string& message) { errorMessage = message; }
+	inline std::string getErrorMessage() const { return errorMessage; }
+
+private:
 	// properties
 	std::unique_ptr<float[]> heightmap = nullptr;
 	int width = 0;
 	int height = 0;
+	std::string errorMessage;
 };
